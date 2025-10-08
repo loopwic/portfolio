05fcf541c4e71705b249bfe53fea07a7802b9689
mkh <me@loopwic.com>
2025-10-08 16:30:06 +0800

    fix: update blog URLs in sitemap for consistency

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,19 +9,19 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: "https://loopwic.com/blogs",
+      url: "https://loopwic.com/blog",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
     },
     {
-      url: "https://loopwic.com/blogs/configure-alacritty-from-scratch",
+      url: "https://loopwic.com/blog/configure-alacritty-from-scratch",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.5,
     },
     {
-      url: "https://loopwic.com/blogs/build-personal-blog-with-nextjs-and-mdx",
+      url: "https://loopwic.com/blog/build-personal-blog-with-nextjs-and-mdx",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.5,
